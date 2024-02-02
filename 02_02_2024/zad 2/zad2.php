1fc8ec093b9adf074f04b7e38990ef503d8895e3
Vaiper <112874836+Happyzaba@users.noreply.github.com>
2024-02-02 11:51:02 +0100

    Add files via upload

---
+++ 02_02_2024/zad 2/zad2.php
@@ -0,0 +1,9 @@
+<?php
+    echo "Dziękujemy! Rejestacja przebiegła pomyślnie". "<br>";
+
+    echo "Wprowadzone dane:<br>";
+
+    echo "Imie:".trim($_GET['imie'])."<br>";
+ 
+    echo "Nazwisko:".trim($_GET['nazwisko'])."<br>";
+?>
---
+++ 02_02_2024/zad 3/zad3.php
@@ -0,0 +1,9 @@
+<?php
+    echo "Dziękujemy! Rejestacja przebiegła pomyślnie". "<br>";
+
+    echo "Wprowadzone dane:<br>";
+
+    echo "Imie:".trim($_POST['imie'])."<br>";
+ 
+    echo "Nazwisko:".trim($_POST['nazwisko'])."<br>";
+?>
